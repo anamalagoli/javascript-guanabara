6650ed60f1ca5a3e5802b2ba6980c5a334a9962b
anamalagoli <anacarolini.malagoli@gmail.com>
2026-03-27 17:22:56 -0300

    cores

--- ex014/script.js
+++ ex014/script.js
@@ -5,14 +5,15 @@ function carregar() {
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
 
-    if (hora < 6) {
-        img.src = "fotonoite.png"
-    } else if (hora < 12) {
+    if (hora >= 6 && < 12) {
         img.src = "fotomanha.png"
+        document.body.style.background = "#edeeed"
     } else if (hora < 18) {
         img.src = "fototarde.png"
+        document.body.style.background = "#a2564c"
     } else {
         img.src = "fotonoite.png"
+        document.body.style.background = "#30494d"
     }
 }
 
