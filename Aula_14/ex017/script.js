1809fa964dfea0a2af405d61a7927e29eda13fc0
anamalagoli <anacarolini.malagoli@gmail.com>
2026-04-13 14:01:05 -0300

    tabuadateste

--- Aula_14/ex017/script.js
+++ Aula_14/ex017/script.js
@@ -1,8 +1,18 @@
-function Tabuada() {
-    let res = document.getElementById('res')
+function Gerar() {
+    let tab = document.getElementById('tabuada')
     let num = document.getElementById('txtnum')
 
-    for (let c = num; c <= 10; c ++) {
-        res.innerHTML = ` ${num} x 1 = ${c}`
+    if (num.value.lenght == 0) {
+        alert("Por favor, digite um número!")
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ""
+
+        for (let c = 1; c <= 10; c ++) {
+        let item = document.createElement("option")
+        item.text = `${n} x ${c} = ${n * c}`
+
+        tab.appendChild(item)
+        }
     }
 }
