89d8f9043c4e2c459dc5702793ba393d04659856
anamalagoli <anacarolini.malagoli@gmail.com>
2026-04-13 13:46:22 -0300

    teste

---
+++ Aula_14/ex017/script.js
@@ -0,0 +1,7 @@
+function Tabuada() {
+    let res = document.getElementById('res')
+    let num = document.getElementById('txtnum')
+
+    for (let c = num; c <= 10; c ++) {
+        res.innerHTML = ` ${num} x 1 = ${c}`
+}
