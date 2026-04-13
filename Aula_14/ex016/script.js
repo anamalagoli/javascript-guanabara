b95c246e8957f11ba99f531dd8c47886faf1e10b
anamalagoli <anacarolini.malagoli@gmail.com>
2026-04-13 10:53:14 -0300

    teste

--- Aula_14/ex016/script.js
+++ Aula_14/ex016/script.js
@@ -7,25 +7,25 @@ function Enviar() {
     if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
         alert('Digite um número válido')
     } else {
-        let inicio = Number(inicio)
-        let fim = Number(fim)
-        let passo = Number(passo)
-                
-        if (passo <= 0) {
-        alert("Passo Inválido! Considerando passo = 1")
-        passo = 1
-        }
-
+        let i = Number(inicio)
+        let f = Number(fim)
+        let p = Number(passo)
+        
         res.innerHTML = ""
 
-        if (inicio < fim) {
+        if (p <= 0) {
+        alert("Passo Inválido! Considerando passo = 1")
+        passo = 1
+        }     
+
+        if (i < f) {
             //contagem crescente
-            for (var c = inicio; c <= fim; c += passo) {
+            for (var c = i; c <= f; c += p) {
                 res.innerHTML += `${c} \u{1F449} `
             }
         } else { 
             //contagem descrente
-            for (var c = fim; c <= inicio; c += passo) {
+            for (var c = i; c >= f; c += p) {
                 res.innerHTML += `${c} \u{1F449} `
             }
         }
