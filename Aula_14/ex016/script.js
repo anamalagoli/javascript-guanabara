b64dd4d3a0517b3ced8c79bc5340b7090566429d
anamalagoli <anacarolini.malagoli@gmail.com>
2026-04-13 10:29:30 -0300

    teste

--- Aula_14/ex016/script.js
+++ Aula_14/ex016/script.js
@@ -1,20 +1,12 @@
 function Enviar() {
     let res = document.getElementById("resultado")
-    let inicio = Number(document.getElementById("idinicio").value)
-    let fim = Number(document.getElementById("idfim").value)
-    let passo = Number(document.getElementById("idpasso").value)
+    let inicio = document.getElementById("idinicio").value
+    let fim = document.getElementById("idfim").value
+    let passo = document.getElementById("idpasso").value
 
-    if (passo <= 0) {
-        alert("Passo Inválido! Considerando passo = 1")
-        passo = 1
+    if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
+        alert('Digite um número válido')
+    } else {
+        alert('Tudo OK')
     }
-
-    res.innerHTML = ""
-
-    for (var c = inicio; c <= fim; c += passo) {
-        res.innerHTML += `${c} \u{1F449} `
-    }
-
-    res.innerHTML += "fim \u{1F3C1}"
 }
-
