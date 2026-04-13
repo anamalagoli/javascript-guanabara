5d58eef521559f0fbbb1659138bc7f509f29a469
anamalagoli <anacarolini.malagoli@gmail.com>
2026-04-13 14:03:12 -0300

    ajuste

--- Aula_14/ex017/script.js
+++ Aula_14/ex017/script.js
@@ -11,7 +11,6 @@ function Gerar() {
         for (let c = 1; c <= 10; c ++) {
         let item = document.createElement("option")
         item.text = `${n} x ${c} = ${n * c}`
-
         tab.appendChild(item)
         }
     }
