1b0ec1f9723022c72e4d58defad3e4c8bb204f17
anamalagoli <anacarolini.malagoli@gmail.com>
2026-04-20 11:17:12 -0300

    ajustesfinais

--- Aula_16/script.js
+++ Aula_16/script.js
@@ -4,30 +4,39 @@ let res = document.getElementById('res')
 let tab = document.getElementById('tab')
 let inputnum = document.getElementById('txtnum')
 
+//funções auxiliares
+function numeroValido(n) {
+    return n >= 1 && n <= 100
+}
+
+function jaExiste(n, lista) {
+    return lista.includes(n)
+}
+
 function adicionar() {
     let num = Number(inputnum.value)
 
-    if (inputnum.value.length == 0) {
-        alert('Por favor, digite um número.')
-    } else if (num < 1 || num > 100) {
-        alert('Por favor, digite um número entre 1 e 100.')
-    } else if (valores.includes(num)) {
-        alert('Esse número já foi adicionado!')
+    if (!inputnum.value) {
+        res.innerHTML = `<p style="color:red;">Por favor, digite um número.</p>`
+    } else if (!numeroValido(num)) {
+        res.innerHTML = `<p style="color:red;">Digite um número entre 1 e 100.</p>`
+    } else if (jaExiste(num, valores)) {
+        res.innerHTML = `<p style="color:red;">Esse número já foi adicionado!</p>`
     } else {
         valores.push(num)
 
         let item = document.createElement('option')
         item.text = `Valor ${num} adicionado.`
         tab.appendChild(item)
+        res.innerHTML = ""
     }
     inputnum.value = ""
     inputnum.focus()
-    res.innerHTML = ""
 }
 
 function finalizar() {
     if (valores.length == 0) {
-        alert('Adicione valores a lista antes de finalizar!')
+        res.innerHTML = `<p style="color:red;">Adicione valores antes de finalizar!</p>`
     } else {
         let maior = Math.max(...valores) //valor máximo
         let menor = Math.min(...valores) //valor mínimo
@@ -54,6 +63,9 @@ function limpar() {
     tab.options.length = 0   // limpa o select
     valores = []             // limpa o array
     res.innerHTML = ""       // limpa o resultado (opcional)
+
+    inputnum.value = ""
+    inputnum.focus()
 }
 
 
