5ccd2f7bc904ecc1a5a77ba17e69ee9909bffadc
anamalagoli <anacarolini.malagoli@gmail.com>
2026-04-20 10:04:15 -0300

    pontos

--- Aula_16/script.js
+++ Aula_16/script.js
@@ -39,8 +39,8 @@ function finalizar() {
     res.innerHTML = `<p>Ao todo, temos ${valores.length} números cadastrados.</p>` 
     res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>` 
     res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>` 
-    res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
-    res.innerHTML += `<p>A média dos valores digitados é  ${media}</p>`
+    res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+    res.innerHTML += `<p>A média dos valores digitados é  ${media}.</p>`
 }
 
 
